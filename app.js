f0885d4752c489a11e42788f9691a5441e244a2c
Eduardo <eduardo25082002@gmail.com>
2025-03-13 18:45:02 -0300

    Terceiro Commit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-let titulo = document.querySelector('h1');
+let titulo = document.querySelector('h1'); // Selecionar uma Tag do HTML
 titulo.innerHTML = 'Jogo do Número Secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um Número entre 1 e 10';
+
+function verificarChute(){
+    console.log("O botão foi pressionado!")
+}
