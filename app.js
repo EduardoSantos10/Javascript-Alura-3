6caee49cce5b5d9d2fa940b3ad01da2a375f89e8
Eduardo <eduardo25082002@gmail.com>
2025-03-17 18:24:04 -0300

    Quarto Commit

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-let titulo = document.querySelector('h1'); // Selecionar uma Tag do HTML
-titulo.innerHTML = 'Jogo do Número Secreto';
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um Número entre 1 e 10';
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1','Jogo do Numero Secreto');
+exibirTextoNaTela('p','Escolha um numero entre 1 e 10')
 
 function verificarChute(){
-    console.log("O botão foi pressionado!")
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+
 }
